64a92835931a8cc4b69ce5df6a5fae8ed3d87df2
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-16 21:43:25 +0530

    Update code

--- app/src/main/java/com/example/beginnerjava/MainActivity.java
+++ app/src/main/java/com/example/beginnerjava/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.beginnerjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    
+    TextView text;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnOK);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = findViewById(R.id.txtBeginner);
+                text.setText("Welcome to Java with Android Studio");
+            }
+        });
     }
 }
